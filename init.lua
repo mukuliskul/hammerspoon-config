5c9192934456d9b85985d42cec4a14d1df6b0705
Mukul Sharma <mukulsharma.jan11@gmail.com>
2025-05-02 17:44:10 -0400

    update shortcuts

--- init.lua
+++ init.lua
@@ -3,13 +3,11 @@ hs.loadSpoon("SpoonInstall")
 -- Install AppLauncher
 spoon.SpoonInstall:andUse("AppLauncher", {
 	hotkeys = {
-		c = "Calendar",
-		d = "Discord",
-		f = "Firefox Developer Edition",
-		n = "Notes",
-		p = "1Password 7",
-		r = "Reeder",
-		t = "Kitty",
-		z = "Zoom.us",
+		g = "ChatGPT",
+		k = "Slack",
+		l = "Linear",
+		o = "Outlook",
+		t = "WezTerm",
+		x = "Google Chrome",
 	},
 })
