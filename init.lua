e0505c8dfbc2fab9cce2e258a07524aa1a5f87fd
Mukul Sharma <mukulsharma.jan11@gmail.com>
2025-05-02 17:55:16 -0400

    add window tile management

--- init.lua
+++ init.lua
@@ -11,3 +11,27 @@ spoon.SpoonInstall:andUse("AppLauncher", {
 		x = "Google Chrome",
 	},
 })
+
+-- Install WindowHalfsAndThirds
+spoon.SpoonInstall:andUse("WindowHalfsAndThirds", {
+	hotkeys = {
+		left_half = { { "ctrl", "cmd" }, "Left" },
+		right_half = { { "ctrl", "cmd" }, "Right" },
+		top_half = { { "ctrl", "cmd" }, "Up" },
+		bottom_half = { { "ctrl", "cmd" }, "Down" },
+		third_left = { { "ctrl", "alt" }, "Left" },
+		third_right = { { "ctrl", "alt" }, "Right" },
+		third_up = { { "ctrl", "alt" }, "Up" },
+		third_down = { { "ctrl", "alt" }, "Down" },
+		top_left = { { "ctrl", "cmd" }, "1" },
+		top_right = { { "ctrl", "cmd" }, "2" },
+		bottom_left = { { "ctrl", "cmd" }, "3" },
+		bottom_right = { { "ctrl", "cmd" }, "4" },
+		max_toggle = { { "ctrl", "alt", "cmd" }, "f" },
+		max = { { "ctrl", "alt", "cmd" }, "Up" },
+		undo = { { "alt", "cmd" }, "z" },
+		center = { { "alt", "cmd" }, "c" },
+		larger = { { "alt", "cmd", "shift" }, "Right" },
+		smaller = { { "alt", "cmd", "shift" }, "Left" },
+	},
+})
